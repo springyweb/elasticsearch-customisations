2da20cea8a56cc7476a7a098ae97b57de6b90598
Si Hutchinson <si@springyweb.com>
2012-01-23 17:53:42 +0000

    pmd fixes

--- src/main/java/com/springyweb/elasticsearch/index/analysis/TokenCountFilter.java
+++ src/main/java/com/springyweb/elasticsearch/index/analysis/TokenCountFilter.java
@@ -12,35 +12,44 @@ import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 /**
  * @author si<si@springyweb.com>
  * 
+ * Prepends the count of the token in the stream to the term stored in the index 
+ * 
  */
 public class TokenCountFilter extends TokenFilter {
 
 	private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
-	private int depth = 0;
+	private int count = 0;
 	
 	public TokenCountFilter(final TokenStream input) {
 		super(input);
 	}
 	
 	/**
-	 * Prepend the value of ${depth} to each token and increment $depth
+	 * Prepend the value of ${count} to each token and increment ${count}
 	 */
 	@Override
 	public final boolean incrementToken() throws IOException {
 		boolean increment = false;
 		if (input.incrementToken()) {
-			final char[] depth = String.valueOf(this.depth++).toCharArray();
-			termAtt.resizeBuffer(termAtt.length() + depth.length);
-			termAtt.setLength(termAtt.length() + depth.length);
-			System.arraycopy(termAtt.buffer(), 0, termAtt.buffer(), depth.length, termAtt.length());
-			System.arraycopy(depth, 0, termAtt.buffer(), 0, depth.length);
+			final char[] count = String.valueOf(this.count++).toCharArray();
+			termAtt.resizeBuffer(termAtt.length() + count.length);
+			termAtt.setLength(termAtt.length() + count.length);
+			System.arraycopy(termAtt.buffer(), 0, termAtt.buffer(), count.length, termAtt.length());
+			System.arraycopy(count, 0, termAtt.buffer(), 0, count.length);
 			increment = true;
 		}
 		return increment;
 	}
 	
 	@Override 
-	public void end() {
-		depth = 0;
+	public void end() throws IOException {
+		super.end();
+		count = 0;
 	}
-}	
+	
+	@Override
+	public void reset() throws IOException {
+		super.reset();
+		count = 0;
+	}
+}
